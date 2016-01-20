c46d28ffa5ca78952bcaa5a2c615ff3e55c8382c
imranariffin <imranariffin@yahoo.com>
2016-01-19 23:09:35 -0500

    retry heroku deploy: use port number 4000

--- app/app.js
+++ app/app.js
@@ -29,10 +29,6 @@ config(['$routeProvider', function($routeProvider) {
   		templateUrl: 'views/speakrs.html',
   		controller: 'SpeakrsCtrl'
   	}).
-    // when('/typeform', {
-    //   templateUrl: 'views/typeform.html',
-    //   controller: 'TypeformCtrl'
-    // }).
 
   	/* try CORS from local server api */
   	when('/get-cors', {
